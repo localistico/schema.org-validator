6f929df0bfa8e581596dd92747c04f0d2f9084c0
felixem <felixsanblas@gmail.com>
2014-08-14 14:14:39 +0200

    Passing Rubocop's style tests

--- app/models/nokogiri_parser.rb
+++ app/models/nokogiri_parser.rb
@@ -1,3 +1,4 @@
+# Encoding: utf-8
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
@@ -6,67 +7,61 @@ require 'open-uri'
 class HtmlParser
   # This class needs the adress string of the webpage
   def initialize(webadress)
-    @adress=webadress
+    @adress = webadress
     begin
-    @page = Nokogiri::HTML(open(@adress))
+      @page = Nokogiri::HTML(open(@adress))
     rescue
       raise("Webpage #{@adress} is not available")
     end
   end
-  
+
   # HTML code of the webpage
   def inspect
     "Parser of #{@adress}:\n#{@page}"
   end
-  
+
   # CSS method filters tags
   def css(string)
     @page.css(string)
   end
-  
+
   # CSS method that builds the hierarchical structure of the tags in MicroData
-  def analyseMicroData
-    
+  def analyse_micro_data
     # Hash table of results
-    hashresults={}
-    
+    hashresults = {}
+
     # Obtain all the schema main itemscopes
-    self.css("div[itemscope]").select{|item| item['itemprop']==nil}.each{
-    |itemscope| 
-    
-    hashresults.merge!(analyseItemscopeMicroData(itemscope))
-    
-    }
-    
-    return hashresults
-    
-  end
-  
-  #Internal method that inspects the hierarchical structure of a itemscope
-  def analyseItemscopeMicroData(itemscope)
-    
-      hashresults={}
-      subclasses={}
-      
-      # Analyse subscopes
-      itemscope.css('div').each do |itemprop|
-      subclasses[itemprop['itemprop']]=analyseItemscopeMicroData(itemprop)
-      end
-    
-      directproperties={}
-      # Obtain direct properties of the main itemscope
-      itemscope.css('span').each do |itemprop|
-      directproperties[itemprop['itemprop']]=analysePropertieMicroData(itemprop)
-      end
-      
-      # Merge filtered results
-      hashresults["#{itemscope['itemtype']}"]=subclasses.merge(directproperties)
-      return hashresults
-  end
-  
-  # Internal method that inspects the values of final properties
-    def analysePropertieMicroData(itemprop)
-      itemprop.text
+    css('div[itemscope]').select { |item| item['itemprop'].nil? }.each do
+      |itemscope|
+      hashresults.merge!(analyse_itemscope_microdata(itemscope))
     end
-  
+
+    hashresults
+  end
+
+  # Internal method that inspects the hierarchical structure of a itemscope
+  def analyse_itemscope_microdata(itemscope)
+    hashresults, subclasses, directproperties = {}, {}, {}
+
+    # Analyse subscopes
+    itemscope.css('div').each do |itemprop|
+      subclasses[itemprop['itemprop']] = analyse_itemscope_microdata(itemprop)
+    end
+
+    # Obtain direct properties of the main itemscope
+    itemscope.css('span').each do |itemprop|
+      directproperties[itemprop['itemprop']] =
+        analyse_propertie_microdata(itemprop)
+    end
+
+    # Merge filtered results
+    hashresults["#{itemscope['itemtype']}"] =
+    subclasses.merge(directproperties)
+    hashresults
+  end
+
+  # Internal method that inspects the values of final properties
+  def analyse_propertie_microdata(itemprop)
+    itemprop.text.gsub(/[\n\r\t]/, '').gsub(/\s\s*/, ' ').rstrip.lstrip
+  end
 end
--- test/models/test_html_parser.rb
+++ test/models/test_html_parser.rb
@@ -1,14 +1,15 @@
+# Encoding: utf-8
 require '../../app/models/nokogiri_parser.rb'
 
 # Main code
 if __FILE__ == $PROGRAM_NAME
   begin
-  parser = HtmlParser.new("../fixtures/webpage_demo.html")
-  puts parser.analyseMicroData
-  
-  rescue Exception => e
-    puts e.message
-    puts e.backtrace.inspect
+    parser = HtmlParser.new('../fixtures/webpage_demo.html')
+    puts parser.analyse_micro_data
+
+    rescue => e
+      puts e.message
+      puts e.backtrace.inspect
   end
 
 end
