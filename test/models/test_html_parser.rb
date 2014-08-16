a141fbb1004e77461639316a82faf7bbbecc1108
felixem <felixsanblas@gmail.com>
2014-08-16 17:17:55 +0200

    YAML fixture file for tests

--- test/models/test_html_parser.rb
+++ test/models/test_html_parser.rb
@@ -1,13 +1,16 @@
 # Encoding: utf-8
 require '../../app/models/html_parser.rb'
 require 'nokogiri'
+require 'yaml'
 
 # Main code
 if __FILE__ == $PROGRAM_NAME
   begin
     parser = HtmlParser.new('../fixtures/webpage_demo.html')
     puts parser.analyse_micro_data
-
+    puts YAML.load_file('../fixtures/webparsed.yml')
+    
+    
     rescue => e
       puts e.message
       puts e.backtrace.inspect
