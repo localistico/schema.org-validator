a0782558d6503f23ff567cf88fef70111aa963ff
felixem <felixsanblas@gmail.com>
2014-08-19 21:57:46 +0200

    RDFa Markup allowed. RDFa test suite.

--- app/models/html_parser.rb
+++ app/models/html_parser.rb
@@ -7,9 +7,9 @@ class HtmlParser
   attr_reader :hash
   # This class needs the adress string of the webpage
   def initialize(webadress)
-    @adress = webadress
-    @page = Nokogiri::HTML(open(@adress))
-    @hash = analyse_microdata
+    @address = webadress
+    @page = Nokogiri::HTML(open(@address))
+    @hash = analyse_data
   end
   # HTML code of the webpage
   def inspect
@@ -26,8 +26,27 @@ class HtmlParser
 
   private
 
+  # Decide how to analyse the HTML structure
+  def analyse_data
+    if css('[itemscope]:not([itemprop])').size > 0
+      MicrodataExtraction.new(@page).analyse_data
+    else
+      RDFaExtraction.new(@page).analyse_data
+    end
+  end
+end
+# Class that extracts schema information of Microdata
+class MicrodataExtraction
+  # This class needs Nokogiri's HTML object
+  def initialize(page)
+    @page = page
+  end
+  # CSS method filters tags
+  def css(string)
+    @page.css(string)
+  end
   # CSS method that builds the hierarchical structure of the tags in MicroData
-  def analyse_microdata
+  def analyse_data
     # Hash table of results
     hashresults = {}
     # Obtain all the schema main itemscopes
@@ -36,6 +55,8 @@ class HtmlParser
     end
     hashresults
   end
+
+  private
 
   # Internal method that inspects the hierarchical structure of a itemscope
   def analyse_itemscope_microdata(itemscope)
@@ -63,12 +84,11 @@ class HtmlParser
   def subscope?(item)
     !item['itemscope'].nil?
   end
-  # Verify if an item is a direct property
-  def directproperty?(item)
+  # Verify if an item is a direct microdata property
+  def microdata_directproperty?(item)
     !item['itemprop'].nil? && !subscope?(item)
   end
-
-  # Internal method that inspects the values of final properties
+  # Internal method that inspects the values of final microdata properties
   def analyse_dirpropertie_microdata(itemprop)
     if !itemprop.text.eql?('')
       itemprop.text.gsub(/[\n\r\t]/, '').gsub(/\s\s*/, ' ').rstrip.lstrip
@@ -77,8 +97,8 @@ class HtmlParser
       itemprop['datetime']
     end
   end
-  # Internal method that process direct properties
-  def process_directpropertie(child, partialresults)
+  # Internal method that process microdata direct properties
+  def process_directpropertie_microdata(child, partialresults)
     results = partialresults[child['itemprop']] || []
     results = [results] unless results.is_a?(Array)
     results << analyse_dirpropertie_microdata(child)
@@ -88,7 +108,7 @@ class HtmlParser
       partialresults[child['itemprop']] = results.first
     end
   end
-  # Grow (or not) datatree depending on schema tag
+  # Grow (or not) datatree depending on microdata's schema tag
   def datatree_grow_microdata(itemscope, partialresults)
     # Ensure the item is a schema tag
     if !(itemscope['itemtype'].to_s == '')
@@ -101,7 +121,7 @@ class HtmlParser
       partialresults
     end
   end
-  # Analyse direct descendants
+  # Analyse Microdata's direct descendants
   def analyse_dirdescendants_microdata(itemscope)
     # Get the direct descendents
     partialresults = {}
@@ -110,18 +130,130 @@ class HtmlParser
     end
     partialresults
   end
-  # Analyse direct descendant
+  # Analyse microdata's direct descendant
   def analyse_dirdescendant_microdata(child, partialresults)
     # Analyse subscope
     if subscope?(child)
       analyse_subscope_microdata(child, partialresults)
     else
       # Obtain direct properties of the main itemscope
-      if directproperty?(child)
-        process_directpropertie(child, partialresults)
+      if microdata_directproperty?(child)
+        process_directpropertie_microdata(child, partialresults)
       else
         # Look for another properties covered by another kind of tags (<h1>)
         partialresults.merge!(analyse_anotpropertie_microdata(child))
+      end
+    end
+  end
+end
+# Class that extracts schema information of RDFa
+class RDFaExtraction
+  # This class needs Nokogiri's HTML object
+  def initialize(page)
+    @page = page
+    @vocab = 'http://schema.org/'
+  end
+  # CSS method filters tags
+  def css(string)
+    @page.css(string)
+  end
+  # CSS method that builds the hierarchical structure of the tags in RDFa
+  def analyse_data
+    # Hash table of results
+    hashresults = {}
+    # Obtain all the schema main types
+    css("[vocab='#{@vocab}']:not([property])").each do |typeof|
+      hashresults.merge!(analyse_typeof_rdfa(typeof))
+    end
+    hashresults
+  end
+
+  private
+
+  # Internal method that inspects the hierarchical structure of a type
+  def analyse_typeof_rdfa(typeof)
+    # Get the direct descendents
+    partialresults = analyse_dirdescendants_rdfa(typeof)
+    # Add tag if the item is a schema tag
+    datatree_grow_rdfa(typeof, partialresults)
+  end
+  # Analyse subtype with mutual recursion
+  def analyse_subtype_rdfa(property, partialresults)
+    results = partialresults[property['property']] || []
+    results = [results] unless results.is_a?(Array)
+    results << analyse_typeof_rdfa(property)
+    if results.size > 1
+      partialresults[property['property']] = results
+    else
+      partialresults[property['property']] = results.first
+    end
+  end
+  # Search another properties of the typeof
+  def analyse_anotpropertie_rdfa(child)
+    analyse_typeof_rdfa(child)
+  end
+  # Verify if an item is a subtype
+  def subtype?(item)
+    !item['typeof'].nil?
+  end
+  # Verify if an item is a direct rdfa property
+  def rdfa_directproperty?(item)
+    !item['property'].nil? && !subtype?(item)
+  end
+  # Internal method that inspects the values of final rdfa properties
+  def analyse_dirpropertie_rdfa(property)
+    if !property.text.eql?('')
+      property.text.gsub(/[\n\r\t]/, '').gsub(/\s\s*/, ' ').rstrip.lstrip
+    else
+      return property['content'] if property['content']
+      property['datetime']
+    end
+  end
+  # Internal method that process rdfa direct properties
+  def process_directpropertie_rdfa(child, partialresults)
+    results = partialresults[child['property']] || []
+    results = [results] unless results.is_a?(Array)
+    results << analyse_dirpropertie_rdfa(child)
+    if results.size > 1
+      partialresults[child['property']] = results
+    else
+      partialresults[child['property']] = results.first
+    end
+  end
+  # Grow (or not) datatree depending on RDFA's schema tag
+  def datatree_grow_rdfa(typeof, partialresults)
+    # Ensure the item is a schema tag
+    if !(typeof['typeof'].to_s == '')
+      # Add schema tag on hash
+      hashresults = {}
+      hashresults["#{@vocab}#{typeof['typeof']}"] = partialresults
+      hashresults
+    else
+      # If not, data tree doesn't grow
+      partialresults
+    end
+  end
+  # Analyse RDFA's direct descendants
+  def analyse_dirdescendants_rdfa(typeof)
+    # Get the direct descendents
+    partialresults = {}
+    typeof.children.each do |child|
+      analyse_dirdescendant_rdfa(child, partialresults)
+    end
+    partialresults
+  end
+  # Analyse rdfa's direct descendant
+  def analyse_dirdescendant_rdfa(child, partialresults)
+    # Analyse subscope
+    if subtype?(child)
+      analyse_subtype_rdfa(child, partialresults)
+    else
+      # Obtain direct properties of the main itemscope
+      if rdfa_directproperty?(child)
+        process_directpropertie_rdfa(child, partialresults)
+      else
+        # Look for another properties covered by another kind of tags (<h1>)
+        partialresults.merge!(analyse_anotpropertie_rdfa(child))
       end
     end
   end
--- test/models/html_parser_test.rb
+++ test/models/html_parser_test.rb
@@ -4,6 +4,7 @@ require 'test_helper'
 
 # HtmlParser Test
 class HtmlParserTest < ActiveSupport::TestCase
+  # MICRODATA TESTS
   # LocalBusiness test
   test 'LocalBusiness' do
     parser = HtmlParser.new('test/fixtures/webpage_demo.html')
@@ -145,6 +146,151 @@ class HtmlParserTest < ActiveSupport::TestCase
   # priceRange rest test
   test 'priceRangeRest' do
     parser = HtmlParser.new('test/fixtures/webpage_demo.html')
+    assert_equal parser['http://schema.org/Restaurant']['priceRange'], '$$'
+  end
+  # ----------------------------------------------------------------
+  # RDFa TESTS
+  # LocalBusiness test
+  test 'LocalBusinessRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    assert_equal parser.hash.keys[0], 'http://schema.org/LocalBusiness'
+  end
+  # Name test
+  test 'nameRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    assert_equal parser['http://schema.org/LocalBusiness']['name'],
+                 'Beachwalk Beachwear & Giftware'
+  end
+  # Description test
+  test 'descriptionRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    description = parser['http://schema.org/LocalBusiness']['description']
+    assert_equal description, 'A superb collection of fine gifts and ' \
+                              'clothing to accent your stay in Mexico Beach.'
+  end
+  # Address test
+  test 'addressRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    address = parser['http://schema.org/LocalBusiness']['address']
+    assert_equal address.keys[0], 'http://schema.org/PostalAddress'
+  end
+  # streetAddress test
+  test 'streetAddressRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    address = parser['http://schema.org/LocalBusiness']['address']
+    assert_equal address['http://schema.org/PostalAddress']['streetAddress'],
+                 '3102 Highway 98'
+  end
+  # addressLocality test
+  test 'addressLocalityRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    address = parser['http://schema.org/LocalBusiness']['address']
+    assert_equal address['http://schema.org/PostalAddress']['addressLocality'],
+                 'Mexico Beach'
+  end
+  # addressRegion test
+  test 'addressRegionRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    address = parser['http://schema.org/LocalBusiness']['address']
+    assert_equal address['http://schema.org/PostalAddress']['addressRegion'],
+                 'FL'
+  end
+  # telephone test
+  test 'telephoneRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    assert_equal parser['http://schema.org/LocalBusiness']['telephone'],
+                 '850-648-4200'
+  end
+  # Restaurant test
+  test 'RestaurantRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    assert_equal parser.hash.keys[1], 'http://schema.org/Restaurant'
+  end
+  # Name test
+  test 'nameRestRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    assert_equal parser['http://schema.org/Restaurant']['name'], 'GreatFood'
+  end
+  # Aggregate rating test
+  test 'aggregateRatingRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    rating = parser['http://schema.org/Restaurant']['aggregateRating']
+    assert_equal rating.keys[0], 'http://schema.org/AggregateRating'
+  end
+  # ratingValue test
+  test 'ratingValueRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    rating = parser['http://schema.org/Restaurant']['aggregateRating']
+    assert_equal rating['http://schema.org/AggregateRating']['ratingValue'], '4'
+  end
+  # reviewCount test
+  test 'reviewCountRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    rating = parser['http://schema.org/Restaurant']['aggregateRating']
+    assert_equal rating['http://schema.org/AggregateRating']['reviewCount'],
+                 '250'
+  end
+  # Address rest test
+  test 'addressRestRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    assert_equal parser['http://schema.org/Restaurant']['address'].keys[0],
+                 'http://schema.org/PostalAddress'
+  end
+  # streetAddress rest test
+  test 'streetAddressRestRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    address = parser['http://schema.org/Restaurant']['address']
+    assert_equal address['http://schema.org/PostalAddress']['streetAddress'],
+                 '1901 Lemur Ave'
+  end
+  # addressLocality rest test
+  test 'addressLocalityRestRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    address = parser['http://schema.org/Restaurant']['address']
+    assert_equal address['http://schema.org/PostalAddress']['addressLocality'],
+                 'Sunnyvale'
+  end
+  # addressRegion rest test
+  test 'addressRegionRestRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    address = parser['http://schema.org/Restaurant']['address']
+    assert_equal address['http://schema.org/PostalAddress']['addressRegion'],
+                 'CA'
+  end
+  # postalCode rest test
+  test 'postalCodeRestRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    address = parser['http://schema.org/Restaurant']['address']
+    assert_equal address['http://schema.org/PostalAddress']['postalCode'],
+                 '94086'
+  end
+  # Telephone rest test
+  test 'telephoneRestRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    assert_equal parser['http://schema.org/Restaurant']['telephone'],
+                 '(408) 714-1489'
+  end
+  # url rest test
+  test 'urlRestRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    assert_equal parser['http://schema.org/Restaurant']['url'],
+                 'www.greatfood.com'
+  end
+  # openingHours rest test
+  test 'openingHoursRestRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    assert_equal parser['http://schema.org/Restaurant']['openingHours'],
+                 ['Mo-Sa 11:00-14:30', 'Mo-Th 17:00-21:30', 'Fr-Sa 17:00-22:00']
+  end
+  # servesCuisine rest test
+  test 'servesCuisineRestRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
+    assert_equal parser['http://schema.org/Restaurant']['servesCuisine'],
+                 ['Middle Eastern', 'Mediterranean']
+  end
+  # priceRange rest test
+  test 'priceRangeRestRDFa' do
+    parser = HtmlParser.new('test/fixtures/webpage_rdfa.html')
     assert_equal parser['http://schema.org/Restaurant']['priceRange'], '$$'
   end
 end
