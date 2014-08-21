7e4a22bebb91963a3164c2153261c74cb364bd86
Fran <fjcr88@gmail.com>
2014-08-21 13:26:18 +0200

    Some fixes

--- app/controllers/validator_controller.rb
+++ app/controllers/validator_controller.rb
@@ -1,10 +1,22 @@
 class ValidatorController < ApplicationController
   def index
-     if request.post?
-          @url = params[:url]; #url de la web que se va a analizar
+      @request = false; #control if the user 
+    
+     #when user pushes the submit button, it creates a HtmlParser object to analyse data.
+     if request.post? 
+          @url = params[:url]; #url to analyse        
+          @urlerror = false; #it isn't any error.
           
-          parser = HtmlParser.new(@url);
+          @request = true; #it allow to show the data in the view
           
+          #Controlling exceptions
+          begin
+              @parser = HtmlParser.new(@url);
+          rescue #catch the exception and send error to the view.
+              @urlerror = true;
+          ensure
+ 
+          end
           
      end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'validator/index'
   
-  post "validator/index"
+  post 'validator/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
