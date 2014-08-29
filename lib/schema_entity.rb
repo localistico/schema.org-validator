70bfe0c385ee5581ecd0c9757dbc7f2bdbe71c32
felixem <felixsanblas@gmail.com>
2014-08-29 19:26:31 +0200

    Schema entity fixes

--- lib/schema_entity.rb
+++ lib/schema_entity.rb
@@ -1,14 +1,20 @@
 # Encoding: utf-8
 # Module that represents any kind of Entity
 module SchemaEntity
+  extend ActiveSupport::Concern
+  include ActiveModel::Validations
+  included do
+    attr_accessor :failures
+    validates :failures, absence: true
+  end
   # Initialize the class with its properties
   def initialize(properties = {})
-    @failures = []
     properties.each do |key, value|
       begin
         send(:"#{key}=", value)
       rescue NoMethodError
         # Property doesn't exist
+        @failures ||= []
         @failures << key
       end
     end
