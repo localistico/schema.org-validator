fadaddffd6d13eb08d4e96aa973d18efd470f92e
Fran <fjcr88@gmail.com>
2014-09-07 23:44:13 +0200

    Demand and duration tests

--- lib/schema_entity/demand.rb
+++ lib/schema_entity/demand.rb
@@ -3,6 +3,7 @@ module SchemaEntity
   # Module that represents the entity Demand
   module Demand
     extend ActiveSupport::Concern
+    include Thing
     include ActiveModel::Validations
     included do
     end
---
+++ test/models/demand_entity_test.rb
@@ -0,0 +1,14 @@
+# Encoding: utf-8
+require 'test_helper'
+
+# DemandEntity Test
+class DemandEntityTest < ActiveSupport::TestCase
+  # DemandEntity test
+  test 'demand' do
+  	text = TextEntity.new(value: 'bussiness')
+    hash = { alternate_name: text}
+    newclass = DemandEntity.new(hash)
+    newclass.valid?
+    assert_equal Hash.new, newclass.errors.messages
+  end
+end
